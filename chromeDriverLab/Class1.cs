c582799b7786438a0f3be9cbed514101a7d35101
Egor Zagny <egor.zagny@gmail.com>
2020-05-01 02:03:42 +0300

    add more tests

--- chromeDriverLab/Class1.cs
+++ chromeDriverLab/Class1.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace chromeDriverLab
 {
@@ -36,12 +37,30 @@ namespace chromeDriverLab
             var text = driver.FindElementByCssSelector(".titles h1").GetAttribute("innerText");
             Assert.AreEqual(text, "SUPPORT US");
         }
-        /*[Test]
+        [Test]
         public void TestLinksImages()
         {
-            driver.FindElementById("menu-item-12045").Click();
-            var href = driver.FindElementByCssSelector(".elementor-widget-container a").GetAttribute("href");
-            Assert.AreEqual(href, url + "isengard/");
-        }*/
+            driver.FindElementById("menu-item-11969").Click();
+            var links = driver.FindElementsByCssSelector("#works a");
+            string[] factions = { "isengard", "factions/elves", "factions/mordor" };
+            for(int i = 0; i < factions.Length; i++)
+            {
+                Assert.AreEqual(links[i].GetAttribute("href"), url + factions[i] +'/');
+            }
+        }
+        [Test]
+        public void TestFactionsPage()
+        {
+            driver.FindElementById("menu-item-11969").Click();
+            driver.FindElementByCssSelector("#works a").Click();
+            new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            var sections = driver.FindElementsByClassName("elementor-button-text");
+            string[] names = { "About faction", "Units", "Heroes", "Buildings", "Spellbook"};
+            Assert.AreEqual(sections.Count, names.Length);
+            for (int i = 0; i < names.Length; i++)
+            {
+                Assert.AreEqual(sections[i].GetAttribute("innerText"), names[i]);
+            }
+        }
     }
 }
