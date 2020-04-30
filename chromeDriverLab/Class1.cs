c6b0cdd2ad4d1429d2418187a2682b8fb5b70080
Egor Zagny <egor.zagny@gmail.com>
2020-04-30 23:48:52 +0300

    init lab

---
+++ chromeDriverLab/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace chromeDriverLab
+{
+    [TestFixture]
+    public class Class1
+    {
+        private ChromeDriver driver;
+        private string url = "https://bfmereforged.org/";
+
+        [SetUp]
+        public void Init()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl(url);
+        }
+
+        [TearDown]
+        public void TheEnd()
+        {
+            driver.Close();
+        }
+
+        [Test]
+        public void TestNav()
+        {
+            driver.FindElementById("menu-item-12045").Click();
+            var text = driver.FindElementByCssSelector(".titles h1").GetAttribute("innerText");
+            Assert.AreEqual(text, "SUPPORT US");
+        }
+        /*[Test]
+        public void TestLinksImages()
+        {
+            driver.FindElementById("menu-item-12045").Click();
+            var href = driver.FindElementByCssSelector(".elementor-widget-container a").GetAttribute("href");
+            Assert.AreEqual(href, url + "isengard/");
+        }*/
+    }
+}
